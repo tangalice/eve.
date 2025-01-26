462a2b14dca1ac981e3d1256acbfa22fa4be10ac
Anika Thapar <anikathapar@pal-nat186-85-28.itap.purdue.edu>
2025-01-25 21:17:25 -0500

    product scanner

---
+++ product_scanner/product_detector.py
@@ -0,0 +1,60 @@
+from keras.models import load_model  # TensorFlow is required for Keras to work
+import cv2  # Install opencv-python
+import numpy as np
+
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+# Load the model
+model = load_model("keras_Model.h5", compile=False)
+
+# Load the labels
+class_names = open("labels.txt", "r").readlines()
+
+# CAMERA can be 0 or 1 based on the default camera of your computer
+camera = cv2.VideoCapture(0)
+
+while True:
+    # Grab the webcamera's image.
+    ret, image = camera.read()
+
+    # Resize the raw image into (224-height, 224-width) pixels
+    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+
+    # Show the image in a window
+    cv2.imshow("Webcam Image", image)
+
+    # Make the image a numpy array and reshape it to the model's input shape
+    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+
+    # Normalize the image array
+    image = (image / 127.5) - 1
+
+    # Predict the model
+    prediction = model.predict(image)
+
+    # Get the class index with the highest confidence score
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
+
+    # Print the prediction and confidence score continuously
+    print("Class:", class_name[2:], end="")
+    print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+
+    # Listen to the keyboard for presses
+    keyboard_input = cv2.waitKey(10) & 0xFF  # Increased wait time
+
+    # If the 'a' key is pressed, print the prediction with the highest confidence and break
+    if keyboard_input == ord('a'):
+        print("Highest confidence class: ", class_name[2:], end="")
+        print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+        break  # Exit the loop when 'a' is pressed
+
+    # 27 is the ASCII for the esc key on your keyboard.
+    if keyboard_input == 27:
+        break  # Exit the loop if 'esc' is pressed
+
+# Release the camera and close all OpenCV windows
+camera.release()
+cv2.destroyAllWindows()
